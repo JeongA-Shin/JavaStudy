5cda74077d58e476d8a52bb59c31d9adfd67687a
ShinjeongA <jeonga@khu.ac.kr>
2022-01-22 16:56:39 +0900

    java method

--- src/opentutorials/methods/MethodDemo1.java
+++ src/opentutorials/methods/MethodDemo1.java
@@ -1,2 +1,24 @@
-package opentutorials.methods;public class MethodDemo1 {
+package opentutorials.methods;
+
+public class MethodDemo1 {
+
+    //함수
+    public static void numbering(){
+        int i=0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    //메인함수
+    public static void main(String[] args){
+        numbering(); //함수를 호출함
+    }
+
+
+
 }
+
+
+

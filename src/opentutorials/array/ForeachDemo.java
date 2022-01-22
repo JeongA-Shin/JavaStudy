af77b6d2e86e59c082ef9f31ecf994aa6b1073df
ShinjeongA <jeonga@khu.ac.kr>
2022-01-22 16:55:43 +0900

    java forEach

--- src/opentutorials/array/ForeachDemo.java
+++ src/opentutorials/array/ForeachDemo.java
@@ -1,2 +1,23 @@
-package opentutorials.array;public class ForeachDemo {
+package opentutorials.array;
+
+public class ForeachDemo {
+
+    public static void main(String[] args){
+        String[] members={"최진혁","최유빈","한이람"};
+//        일반적인 for 반복문
+//        for(int i = 0; i < members.length; i++) {
+//            String member = members[i];
+//            System.out.println(member + "이 상담을 받았습니다"); 
+//        }
+
+//        for each 문법 사용해서 돌리기
+        for(String e:members){
+            System.out.println(e);
+        }
+        
+        
+        //배열 선언한는 방법
+        String[] members2=new String[5]; 
+        //5칸 짜리 배열임
+    }
 }

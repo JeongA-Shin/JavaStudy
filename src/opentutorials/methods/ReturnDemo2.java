bd97f9f106f23bfae9a81d3660dbbabfb7ee6612
ShinjeongA <jeonga@khu.ac.kr>
2022-01-22 16:57:03 +0900

    java method - return

--- src/opentutorials/methods/ReturnDemo2.java
+++ src/opentutorials/methods/ReturnDemo2.java
@@ -1,2 +1,33 @@
-package opentutorials.methods;public class ReturnDemo2 {
+package opentutorials.methods;
+
+public class ReturnDemo2 {
+//    public static String num(int i) { //리턴값이 있으면 c++처럼 반환형을 반드시! 명시해줘야 한다!
+//        if(i==0){
+//            return "zero";
+//        } else if(i==1){
+//            return "one";
+//        } else if(i==2){
+//            return "two";
+//        }
+//        return "none";
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(num(1));
+//    }
+
+
+    //반환 자료형이 배열도 가능함!
+    public static String[] getMembers() {
+        String[] myMem = { "최혁", "최빈", "한이" };
+        return myMem; //이렇게 배열 자체를 리턴해줄 수도 있다!
+    }
+
+
+    public static void main(String[] args) {
+        String[] members = getMembers();
+        System.out.println(members[0]);
+    }
+
+
 }
